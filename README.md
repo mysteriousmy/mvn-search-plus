7dcfc1355f202249d8b7ea470fc225d1027fd4e5
mysteriousmy <jackmysterious@outlook.com>
2021-12-03 11:46:29 +0800

    从主仓库迁移

---
+++ README.md
@@ -0,0 +1,4 @@
+# `mvn-search-plus`
+基于
+[mvn-search](https://github.com/erosb/mvn-search)魔改的小工具。只在作者的基础上加了复制gradle导包信息的函数
+后续个人有需求再更新做更多功能。
---
+++ src/index.js
@@ -0,0 +1,183 @@
+const fetch = require("node-fetch");
+const inquirer = require("inquirer");
+const clipboardy = require("clipboardy");
+const ProxyAgent = require("proxy-agent");
+
+const proxyAgent = new ProxyAgent();
+
+let lastSearchResults;
+let searchTerm;
+let dependencyFormat;
+
+function formatDependency(dependency) {
+    switch (dependencyFormat.toLowerCase()) {
+        case 'gradlekts':
+        case 'gradle':
+            return formatDependencyGradle(dependency);
+            break;
+        case 'gradlegroovy':
+            return formatDependencyGradleGroovy(dependency);
+        case 'sbt':
+            return formatDependencySbt(dependency);
+        case 'maven':
+        default:
+            return formatDependencyXml(dependency);
+    }
+}
+
+function formatDependencyXml(depencency) {
+    return `
+        <dependency>
+            <groupId>${depencency.groupId}</groupId>
+            <artifactId>${depencency.artifactId}</artifactId>
+            <version>${depencency.versions[0]}</version>
+        </dependency>
+        `;
+}
+function formatDependencyGradle(depencency) {
+    return `compile group: '${depencency.groupId}', name: '${depencency.artifactId}', version: '${depencency.versions[0]}'`;
+}
+function formatDependencyGradleKotlin(depencency) {
+    return `
+        implementation("${depencency.groupId}:${depencency.artifactId}:${depencency.versions[0]}")
+        `;
+}
+
+function formatDependencyGradleGroovy(depencency) {
+    return `
+        implementation '${depencency.groupId}:${depencency.artifactId}:${depencency.versions[0]}'
+        `;
+}
+
+function formatDependencySbt(depencency) {
+    return `
+        libraryDependencies += "${depencency.groupId}" % "${depencency.artifactId}" % "${depencency.versions[0]}"
+        `;
+}
+
+function versionSearchResponseArrived(resp) {
+    const hits = JSON.parse(resp).response.docs;
+    if (hits.length === 0) {
+        console.log("no result");
+        return;
+    }
+    let needle = hits[0];
+    const filtered = lastSearchResults.filter(e => e.groupId === needle.g && e.artifactId === needle.a);
+    if (filtered.length === 0) {
+        console.warn(`could not find ${needle.groupId}:${needle.artifactId}`);
+        return;
+    }
+    const target = filtered[0];
+    hits.forEach(e => target.versions.push(e.v));
+    console.log(target.versions)
+}
+
+function mvnSearchResponseArrived(resp) {
+    const hits = JSON.parse(resp).response.docs;
+
+    if (hits.length === 0) {
+        console.error(`no results`)
+        newSearch();
+        return;
+    }
+
+    lastSearchResults = hits.map(val => {
+        return {
+            groupId: val.g,
+            artifactId: val.a,
+            packaging: val.p,
+            versions: [val.latestVersion]
+        };
+    });
+
+    const choices = lastSearchResults.map(result => {
+        return {
+            name: result.groupId + ":" + result.artifactId + ":" + result.versions[0],
+            value: result
+        };
+
+    });
+    inquirer.prompt({
+        type: "list",
+        name: "coordinates",
+        "pageSize": 30,
+        choices
+    }).then(answers => {
+        const ans = answers.coordinates;
+        console.log(ans);
+        console.log(formatDependency(ans));
+        inquirer.prompt([
+            {
+                "type": "list",
+                "name": "action",
+                "choices": [
+                    {
+                        "name": "Copy to clipboard",
+                        "value": "copyToClipboard"
+                    },
+                    {
+                        "name": "Copy to clipboardGradle",
+                        "value": "copyToClipboardGradle"
+                    },
+                    {
+                        "name": "Search older versions",
+                        "value": "searchOlderVersions"
+                    },
+                    {
+                        "name": "Start a new search",
+                        "value": "newSearch"
+                    },
+                    {
+                        "name": "quit",
+                        "value": "quit"
+                    }
+                ]
+            }
+        ]).then(answers => {
+            const action = answers.action;
+            switch (action) {
+                case "quit":
+                    break;
+                case "newSearch":
+                    newSearch();
+                    break;
+                case "copyToClipboard":
+                    clipboardy.writeSync(formatDependency(ans))
+                    break;
+                case "copyToClipboardGradle":
+                    dependencyFormat = 'gradle'
+                    clipboardy.writeSync(formatDependency(ans))
+                    break;
+                case "searchOlderVersions":
+                    fetch("https://search.maven.org/solrsearch/select?rows=100&q=g:" + ans.groupId + "+AND+a:" + ans.artifactId + "&core=gav", {agent: proxyAgent})
+                        .then(resp => resp.text())
+                        .then(versionSearchResponseArrived);
+                    break;
+            }
+        });
+    });
+}
+
+function newSearch() {
+    inquirer.prompt([
+        {
+            "type": "input",
+            "name": "search term"
+        }
+    ]).then(answer => startSearch(answer["search term"]));
+}
+
+function startSearch(searchTerm) {
+    fetch("https://search.maven.org/solrsearch/select?rows=100&q=" + searchTerm, {agent: proxyAgent}).then(resp => resp.text())
+    .then(mvnSearchResponseArrived);
+}
+
+export function search(term, format) {
+    searchTerm = term || '';
+    dependencyFormat = format || 'maven';
+    if (searchTerm.trim() === "") {
+        newSearch();
+    } else {
+        startSearch(searchTerm);
+    }
+}
